3f4b1427ae8da8755b2d4f660ac85f9217826c58
burton <burton@spinn3r.com>
2018-07-26 08:12:18 -0700

    this code works now and we properly work with typescirpt...

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+export class MyClass {
+
+    public message: string = "hello world";
+
+}
+
+const myClass = new MyClass();
+document.body.innerHTML = myClass.message;
